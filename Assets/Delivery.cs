2e626ef3f1feafde2d20641b7417e8145be3b152
Ibrahim Yildiz <139260690+ibrahimyildizzz@users.noreply.github.com>
2023-07-11 21:06:16 +0300

    Projects updated.

---
+++ Assets/Delivery.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    bool hasPackage = false;
+    [SerializeField] float destroyDelay = 0.5f;
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+
+    SpriteRenderer spriteRenderer;
+
+    void Start(){
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log("carpti");
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Package" && !hasPackage){
+            Debug.Log("Package picked up");
+            hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, destroyDelay);
+        }
+        if (other.tag == "Customer" && hasPackage){
+            Debug.Log("Delivered package");
+            hasPackage = false;
+            spriteRenderer.color = noPackageColor;
+        }
+    }
+}
---
+++ Assets/Driver.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transform.Rotate(0, 0, 45);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.Rotate(0, 0, -steerAmount); // Left - Right dan dolayi - koymamiz lazim
+        transform.Translate(0, moveAmount, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "SpeedUp"){
+            moveSpeed = boostSpeed;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+            moveSpeed = slowSpeed; 
+    }
+}
